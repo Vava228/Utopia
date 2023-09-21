a2eb5019a8386d061f73e812101f7a5a32d2765f
Yegor Chernov <egor200619@gmail.com>
2023-09-21 22:24:35 +0300

    Vava228 1.1.1 Added slab blocks

--- src/main/java/com/Utopia/utopiamod/mixin/MixinWidget.java
+++ src/main/java/com/Utopia/utopiamod/mixin/MixinWidget.java
@@ -2,8 +2,12 @@ package com.Utopia.utopiamod.mixin;
 
 import net.minecraft.client.gui.widget.Widget;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(Widget.class)
 public class MixinWidget {
+
+    @Inject(at = @At(value = "HEAD"), )
 
 }
