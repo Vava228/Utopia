3b29eb478cccb4d637b5781af03b4314e81ef008
Yegor Chernov <egor200619@gmail.com>
2023-09-24 19:59:20 +0300

    Vava228 1.4 CraftReducing mixin base

---
+++ src/main/java/com/Utopia/utopiamod/CraftingDisableMod.java
@@ -0,0 +1,19 @@
+package com.Utopia.utopiamod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = "utopia")
+public class CraftingDisableMod {
+
+    @SubscribeEvent
+    public static void onCrafting(PlayerEvent.ItemCraftedEvent event) {
+        event.setCanceled(true); // Отменяем событие крафта
+    }
+
+    public CraftingDisableMod() {
+        MinecraftForge.EVENT_BUS.register(this); // Регистрируем обработчик событий
+    }
+}
